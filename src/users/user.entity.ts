3a155d0d222c6b4349d433d31c70908824513acb
Lucas Becerra <lucbece@gmail.com>
2022-08-30 19:08:34 -0300

    first commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,33 @@
+import {
+    AfterInsert,
+    AfterRemove,
+    AfterUpdate,
+    Entity,
+    Column,
+    PrimaryGeneratedColumn
+} from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @AfterInsert()
+    logInsert() {
+        console.log('Inserted User with id', this.id);
+    }
+    @AfterUpdate()
+    logUpdatE() {
+        console.log('Update User with id', this.id);
+    }
+    @AfterRemove()
+    logRemove() {
+        console.log('Remove User with id', this.id);
+    }
+}
