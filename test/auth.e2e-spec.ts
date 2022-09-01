3efcac56522ea917536e42a70d2073e2331e0b7d
Lucas Becerra <lucbece@gmail.com>
2022-09-01 09:55:35 -0300

    update

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -25,6 +25,24 @@ describe('Authentication Syste', () => {
         const { id, email } = res.body;
         expect(id).toBeDefined();
         expect(email).toEqual(email);
-      })
+      });
   });
+
+  // it('signup as a new user then get the currently logged in user', async () => {
+  //   const email = 'asdf@asdf.com';
+
+  //   const res = await request(app.getHttpServer())
+  //   .post('/auth/signup')
+  //   .send({ email, password: 'asdf' })
+  //   .expect(201)
+
+  //   const cookie = res.get('Set-Cookie');
+
+  //   const { body } = await request(app.getHttpServer())
+  //     .get('auth/whoami')
+  //     .set('Cookie', cookie)
+  //     .expect(200)
+
+  //   expect(body.email).toEqual(email);
+  // });
 });
